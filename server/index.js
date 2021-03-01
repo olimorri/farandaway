7242b8be55e5b37d2221446894e4e8714adfa0bb
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-01 21:14:16 +0000

    feat(index.js): fully-working basic server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@
 const express = require('express');
 const app = new express();
 
+//TODO: const router = require('./router');
 const PORT = 3000;
+
+app.use(express.json());
+//TODO: app.use(router); - remove this once router created
 
 (async function () {
   try {
