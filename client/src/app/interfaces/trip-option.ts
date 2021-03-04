a636d59d026620f82899779e066b277e807e9ba1
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-04 11:37:44 +0000

    refactor(interfaces): updated interfaces for trip-option and trip to match what the db will expect

--- client/src/app/interfaces/trip-option.ts
+++ client/src/app/interfaces/trip-option.ts
@@ -1,7 +1,9 @@
 export interface TripOption {
+  title: string;
   destination : string;
   startDate: string;
   nights: number;
   budgetRangeMin: number;
   budgetRangeMax: number;
+  isChosen: boolean;
 }
---
+++ client/src/app/interfaces/trip.ts
@@ -0,0 +1,5 @@
+export interface Trip {
+  tripId:  number,
+  title: string,
+  option: []
+}
