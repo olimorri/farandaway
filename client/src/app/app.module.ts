f216dbb592d52454b1e6f5286a383ba4204b2ddb
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-03-04 15:49:27 +0000

    refactor: included HttpClientModule import

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { CreateTripAccordionComponent } from './create-trip/create-trip-accordio
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
     MaterialModule,
     ReactiveFormsModule
 
